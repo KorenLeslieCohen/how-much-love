feba00bd7dcb05630443e487937748d003f4e8bf
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-11-01 23:08:23 -0400

    rate and stats sass

--- public/javascripts/about.js
+++ public/javascripts/about.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-  $('#clueless1, #clueless2, #clueless3, #clueless4, #clueless5, #clueless6, #clueless7, #clueless8, #clueless9, #help').hide();
+  $('#clueless1, #clueless2, #clueless3, #clueless4, #clueless5, #clueless6, #clueless7, #clueless8, #help').hide();
   var aboutArr = [$('#clueless1').html(), $('#clueless2').html(), $('#clueless3').html(), 
     $('#clueless4').html(), $('#clueless5').html(), $('#clueless6').html(),
-    $('#clueless7').html(), $('#clueless8').html(), $('#clueless9').html(), 
-    $('#help').html()];
+    $('#clueless7').html(), $('#clueless8').html(), $('#help').html()];
   var place = [];
 
   function clueless() {
