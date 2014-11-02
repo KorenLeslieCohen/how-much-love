501e7f6891da38a6d80e44622717a0368157aa22
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-11-01 22:31:49 -0400

    sass about

--- public/javascripts/about.js
+++ public/javascripts/about.js
@@ -1,23 +1,20 @@
 $(document).ready(function() {
-
-  $('#clueless1, #clueless2, #clueless3, #clueless4, #clueless5, #help').hide();
+  $('#clueless1, #clueless2, #clueless3, #clueless4, #clueless5, #clueless6, #clueless7, #clueless8, #clueless9, #help').hide();
   var aboutArr = [$('#clueless1').html(), $('#clueless2').html(), $('#clueless3').html(), 
-    $('#clueless4').html(), $('#clueless5').html(), $('#clueless6').html(), 
-    $('#clueless7').html(), $('#help').html()];
+    $('#clueless4').html(), $('#clueless5').html(), $('#clueless6').html(),
+    $('#clueless7').html(), $('#clueless8').html(), $('#clueless9').html(), 
+    $('#help').html()];
   var place = [];
 
   function clueless() {
     for (var i in aboutArr) {
       place[i] = aboutArr[i];
-      // console.log(aboutArr[i]);
       var max = 5;
       var j = 0;
       setTimeout(function() {
         j += 1;
-        // console.log(j);
         $('.clueless').html(place[j]);
-        // console.log(place[j]);
-      }, 3000 * i);
+      }, 4000 * i);
     }
   }
 
--- public/javascripts/shake.js
+++ public/javascripts/shake.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       "Make him come to you.", "Find an excuse to leave while he's still into the conversation.", 
       "They key is, always have him wanting more.", "Don't be stupid, you can do so much better.",
       "He thinks he's all that.", "Just as long as his you-know-what isn't crooked.", 
-      "Don't sell yourself short.", "You gave him a toothache.", "Do a lap before you commit to a location.",
+      "Don't sell yourself short.", "Give him a toothache.", "Do a lap before you commit to a location.",
       "You two don't make any sense.", "You are the most beautiful girl in Beverly Hills.",
       "You know you're gorgeous, alright? And popular.", "Wine makes people wanna feel, like sexy.",
       "He dresses better than you. What would you bring to the relationship?", "You don't mesh well together.",
@@ -16,7 +16,7 @@ $(document).ready(function() {
       " Legs crossed towards each other. That is an unequivocal sex invite.",
       "Sometimes you have to show a little skin. This reminds boys of being naked, and then they think of sex."];
     var rand = eightArr[Math.floor(Math.random() * eightArr.length)];
-    $('#answer p').html(rand).hide().fadeIn(2000);
+    $('#answer p').html(rand).hide().fadeIn(500);
   });
 });
 
