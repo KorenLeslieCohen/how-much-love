0e429f39ba073e9595a31e09b453e576ba211957
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-11-02 06:24:49 -0500

    shake js

--- public/javascripts/shake.js
+++ public/javascripts/shake.js
@@ -10,13 +10,14 @@ $(document).ready(function() {
       "He thinks he's all that.", "Just as long as his you-know-what isn't crooked.", 
       "Don't sell yourself short.", "He said you gave him a toothache.", "Do a lap before you commit to a location.",
       "You two don't make any sense.", "You are the most beautiful girl in Beverly Hills.",
-      "You know you're gorgeous, alright? And popular.", 
+      "You know you're gorgeous, alright? And popular.", "Send yourself love letters, flowers and candy so he sees how desired you are.",
       "He dresses better than you. What would you bring to the relationship?", "You don't mesh well together.",
       "Don't rely on mirrors; always take polaroids.", "Whenever a boy comes, you should always have something baking.",
       "Don't forget to design a lighting concept.", "Tis a far, far better thing doing stuff for other people.",
       "Anything you can do to draw attention to your mouth is good.", "Okay, okay, so he is kind of a Baldwin.",
       " Legs crossed towards each other. That is an unequivocal sex invite.", "Whenever a boy comes, you should always have something baking.",
-      "Sometimes you have to show a little skin. This reminds boys of being naked, and then they think of sex."];
+      "Sometimes you have to show a little skin. This reminds boys of being naked, and then they think of sex.",
+      "If you strike while the iron is hot, you can have any guy that you want.", "Act like someone is saying something funny."];
     var rand = eightArr[Math.floor(Math.random() * eightArr.length)];
     $('#answer p').html('<p>' + rand + '<p>').hide().fadeIn(800);
   });
